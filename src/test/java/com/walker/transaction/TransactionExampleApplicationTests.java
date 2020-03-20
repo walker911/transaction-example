e69c0b72072a6cdf666c54ea27554f5d2a6f4c68
qinmu <qinmu911@163.com>
2020-03-20 19:07:35 +0800

    initial all

---
+++ src/test/java/com/walker/transaction/TransactionExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walker.transaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
